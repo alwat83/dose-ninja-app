02c9e27b6df19415882db79876448674f160403b
alwat83 <albertlwatson@gmail.com>
2024-05-24 10:04:56 -0700

    Update app.module.ts

--- dose-ninja-app/src/app/app/app.module.ts
+++ dose-ninja-app/src/app/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { firebaseConfig } from '../environments/environment.firebase';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
-
 import { AppComponent } from './app.component';
-import { MedicationComponent } from './components/medication/medication.component';
-import { MedicationService } from './services/medication.service';
+import { MedicationComponent } from './medication/medication.component';
+import { environment } from '../../environments/environment.firebase'; // Corrected path
 
 @NgModule({
   declarations: [
@@ -20,18 +16,13 @@ import { MedicationService } from './services/medication.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireStorageModule,
-    LoggerModule.forRoot({
-      level: NgxLoggerLevel.DEBUG,
-      serverLogLevel: NgxLoggerLevel.ERROR,
-    })
+    LoggerModule.forRoot({ level: NgxLoggerLevel.DEBUG, serverLogLevel: NgxLoggerLevel.ERROR }),
   ],
-  providers: [MedicationService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
